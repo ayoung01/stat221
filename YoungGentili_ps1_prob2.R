3bc3b7d9f55e891defa3a134ec8d495eecd4579d
Albert Tan Young <albert.t.young@gmail.com>
2014-09-12 10:54:36 -0400

    shuffled some files

--- YoungGentili_ps1_prob2.R
+++
@@ -1,8 +0,0 @@
-
-ll <- function(G, theta, X) {
-  
-}
-
-data <- read.csv("data1985_area2.csv", header = TRUE, sep="\t")
-data.matrix <- matrix(data)
-#no.id <- submatrix'(data,,{1})
---
+++ pset1/YoungGentili_ps1_prob2.R
@@ -0,0 +1,7 @@
+
+ll <- function(G, theta, X) {
+  
+}
+
+data = read.delim("data1985_area2.csv", stringsAsFactors=FALSE)
+attach(data)
