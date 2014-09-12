00331330ecf4885eb574f64f6eeb7bf863dd6075
Albert Tan Young <albert.t.young@gmail.com>
2014-09-12 10:39:59 -0400

    1.3 and 1.4 working

--- YoungGentili_ps1_prob1.R
+++ YoungGentili_ps1_prob1.R
@@ -1,4 +1,5 @@
 library(mvtnorm)
+`%+%` <- function(x,y) paste(x,y,sep="")
 
 createCovMat <- function(alpha, beta, dim) {
   mat <- matrix( data = -1*beta, nrow = dim, ncol = dim)
@@ -9,30 +10,33 @@ createCovMat <- function(alpha, beta, dim) {
 }
 
 dlogisticnorm <- function(u,mu,alpha,beta) {
-  cov <- createCovMat( alpha, beta, length(mu))
+  cov <- createCovMat(alpha, beta, length(mu))
   u.trans <- log(u/(1-sum(u)))
-  print( dmvnorm( u.trans, mean = mu, sigma = cov ) )
   res <- 1/(prod(u)*(1-sum(u)))*dmvnorm( u.trans, mean = mu, sigma = cov )
   return( res )
 }
 
 loglikelihood <- function(par, data) {
-  mu <- par[0:(length(par)-2)]
-  print(mu)
-  alpha <- par[length(par)-1]
-  beta <- par[length(par)]
-  return( sum(apply( data, 1, function (x) dlogisticnorm(x, mu, alpha, beta))) )
+  mu <- par[1:3]
+#   print(mu)
+  alpha <- par[4]
+  beta <- par[5]
+  return(prod(apply(data, 1, function(x) dlogisticnorm(x, mu, alpha, beta))))
 }
 
 logisticnorm.mle <- function(U) {
   #mu.init <- numeric(length(U))
   mu.init <- c(0,0,0)
   alpha.init <- 1
-  beta.init <- 1
+  beta.init <- .1
   par.init <- c(mu.init, alpha.init, beta.init)
-  result <- optim( par.init, loglikelihood, data = U )
-  return( list( "mu.hat"=result$par[1], "alpha.hat"=result$par[2], "beta.hat"=result$par[3]) )
+  result <- optim(par.init, loglikelihood, data = U, control=list(fnscale=-1))
+  return(result)
 }
 
-data <- read.table("dataLogisticNorm3D.txt",header=TRUE)
-x <- logisticnorm.mle( data )
+data <- read.table("dataLogisticNorm3D.txt", header=TRUE)
+x <- logisticnorm.mle(data)
+
+print('mu.mle: '%+%x$par[1:3])
+print('alpha.mle: '%+%x$par[4])
+print('beta.mle: '%+%x$par[5])
