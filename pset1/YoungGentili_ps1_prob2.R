8e755b59a5fb452e1a5d25f86803b344fff66856
Albert Tan Young <albert.t.young@gmail.com>
2014-09-24 18:00:29 -0400

    finished pset1

--- pset1/YoungGentili_ps1_prob2.R
+++ pset1/YoungGentili_ps1_prob2.R
@@ -1,8 +1,58 @@
 `%+%` <- function(x,y) paste(x,y,sep="")
 
+# transform G to simplex
+# x: N x 2 matrix
+x_to_u_g = function(x) {
+  sums = apply(x, 1, function(x){
+    1 + exp(x[1]) + exp(x[2])
+  })
+  for (i in 1:nrow(x)) {
+    x[i,1] = exp(x[i,1])/sums[i]
+    x[i,2] = exp(x[i,2])/sums[i]
+  }
+  return(x)
+}
+# transform simplex to G
+# x: N x 2 matrix
+u_to_x_g = function(x) {
+  sums = apply(x, 1, function(x){
+    1 - (x[1] + x[2])
+  })
+  for (i in 1:nrow(x)) {
+    x[i,1] = log(x[i,1]/sums[i])
+    x[i,2] = log(x[i,2]/sums[i])
+  }
+  return(x)
+}
+
+# transform theta to simplex
+# x: list
+x_to_u_theta = function(x) {
+  sums = 1 + sapply(x, function(y){sum(exp(y))})
+  res = list()
+  x = lapply(seq_along(x), function(x.idx) {
+    return(sapply(x[[x.idx]], function(z){
+      exp(z)/sums[x.idx]
+    }))
+  })
+  return(x)
+}
+# transform simplex to theta
+# x: list
+u_to_x_theta = function(x) {
+  sumsiu = 1 - sapply(x, sum)
+  lapply(seq_along(x), function(x.idx){
+    sapply(x[[x.idx]], function(z){
+      log(z/sums[x.idx])
+    })
+  })
+}
+
 # optimize ll function over g
 ll_g = function(par, theta_H, theta_L, X) {
-  G = matrix(par, ncol=2, byrow=TRUE)
+  G = x_to_u_g(matrix(par, ncol=2, byrow=TRUE))
+  theta_H = x_to_u_theta(theta_H)
+  theta_L = x_to_u_theta(theta_L)
   N = dim(G)[1]
   P = length(theta_H)
 
@@ -20,8 +70,10 @@ ll_g = function(par, theta_H, theta_L, X) {
 
 # optimize ll function over theta_H
 ll_H = function(par, G, theta_L, X) {
+  G = x_to_u_g(G)
+  theta_H = x_to_u_theta(relist(par, sk))
+  theta_L = x_to_u_theta(theta_L)
   N = dim(G)[1]
-  theta_H = relist(par, sk)
   P = length(theta_L)
   res = 0
   for (n in 1:N) {
@@ -37,8 +89,10 @@ ll_H = function(par, G, theta_L, X) {
 
 # optimize ll function over theta_L
 ll_L = function(par, G, theta_H, X) {
+  G = x_to_u_g(G)
+  theta_H = x_to_u_theta(theta_H)
+  theta_L = x_to_u_theta(relist(par, sk))
   N = dim(G)[1]
-  theta_L = relist(par, sk)
   P = length(theta_H)
   res = 0
   for (n in 1:N) {
@@ -55,74 +109,62 @@ ll_L = function(par, G, theta_H, X) {
 # Find MLE for (G, Theta) using a coordinate ascent approach, maximizing
 # over parameters in the folLing sequence:
 # g_L,n for n=1,...,N
-# Theta_L,j for j=1,...,J
-# Theta_H,j for j=1,...,J
+# Theta_L,j for j=1,...,P
+# Theta_H,j for j=1,...,P
 gomMLE = function(X, G0, theta0) {
   curr.value = -Inf
+  NUM_ITER = 1
+  EPSILON = 0.1
+  MAX_ITER = 25
   theta_H = theta0$H
   theta_L = theta0$L
-  counter = 1
-  epsilon = 1e-3
 
-  # create ui matrix for G
-  ui.G = matrix(0, nrow=nrow(G0), ncol=2*nrow(G0))
-  j = 1
-  for (i in 1:nrow(G0)) {
-    ui.G[i, j] = -1
-    ui.G[i, j + 1] = -1
-    j = j + 2
-  }
-  ci.G = rep(-1, nrow(G0))
+  # transform initial G and theta into the real space to run optim
 
-  # create ui matrix for Theta
-  n.levels = length(theta0$L)
-  n.theta = length(unlist(theta0$L))
-  len.levels = sapply(theta0$L, length)
-  ui.theta = matrix(0, nrow=n.levels, ncol=n.theta)
-
-  j = 1
-  i = 1
-  for (len.level in len.levels) {
-    for (k in 1:len.level) {
-      ui.theta[i, j] = -1
-      j = j + 1
-    }
-    i = i + 1
-  }
-  ci.theta = rep(-1, n.levels)
-
+  G0 = u_to_x_g(G0)
+  theta_H = u_to_x_theta(theta0$H)
+  theta_L = u_to_x_theta(theta0$L)
 
   repeat  {
-    print('Iteration: '%+%counter)
+    trace = 3
+    lower = 0
+    upper = 100
+    print('ITERATION: '%+%NUM_ITER)
     # Need to flatten all parameters before passing to optim
     print('Optimizing over G...')
-    res = constrOptim(theta=as.vector(t(G0)), f=ll_g, grad=NULL, ui=ui.G, ci=ci.G,
-                control=list(fnscale=-1,trace=1), X=X, theta_H=theta_H, theta_L=theta_L)
+    res = optim(par=as.vector(t(G0)), fn=ll_g, method='L-BFGS-B', lower=lower, upper=upper,
+                control=list(fnscale=-1,trace=trace), X=X, theta_H=theta_H, theta_L=theta_L)
     G0 = matrix(res$par, ncol=2, byrow=TRUE)
     print(res$value)
 
     print('Optimizing over theta_L...')
-    res = constrOptim(theta=unlist(theta_L), f=ll_L, grad=NULL, ui=ui.theta, ci=ci.theta,
-                control=list(fnscale=-1,trace=1), X=X, G=G0, theta_H=theta_H)
+    res = optim(par=unlist(theta_L), fn=ll_L, method='L-BFGS-B', lower=lower, upper=upper,
+                control=list(fnscale=-1,trace=trace), X=X, G=G0, theta_H=theta_H)
     theta_L = relist(res$par, sk)
     print(res$value)
 
     print('Optimizing over theta_H...')
-    res = constrOptim(theta=unlist(theta_H), f=ll_H, grad=NULL, ui=ui.theta, ci=ci.theta,
-                control=list(fnscale=-1,trace=1), X=X, G=G0, theta_L=theta_L)
+    res = optim(par=unlist(theta_H), fn=ll_H, method='L-BFGS-B', lower=lower, upper=upper,
+                control=list(fnscale=-1,trace=trace), X=X, G=G0, theta_L=theta_L)
     theta_H = relist(res$par, sk)
 
     # Stop coordinate ascent if our log-likelihood function stops increasing by a factor of > epsilon
-    if (res$value - curr.value < epsilon) {
+    # or if we exceed a maximum number of iterations
+    if (res$value - curr.value < EPSILON || NUM_ITER > MAX_ITER) {
       print('And we are done!')
       print(c(curr.value, res$value))
       break
     }
     curr.value = res$value
     print("Current value: "%+%curr.value)
-    counter = counter + 1
+    NUM_ITER = NUM_ITER + 1
   }
-  return(list(G.hat=G0, theta.hat=list(H=theta_H, L=theta_L), maxlik=curr.value))
+  G0 = x_to_u_g(G0)
+  theta_H = x_to_u_theta(theta_H)
+  theta_L = x_to_u_theta(theta_L)
+  mles = list(G.hat=G0, theta.hat=list(H=theta_H, L=theta_L), maxlik=curr.value)
+  save(mles, file='mles.RData')
+  return(mles)
 }
 
 data1985 = read.delim("data1985_area2.csv", stringsAsFactors=FALSE)
@@ -153,7 +195,7 @@ theta0$H[[46]] = c(.01,.35,.35,.05,.08,.06,.09,.01)
 theta0$H[[49]] = c(.44,.01,.11,.43,.01)
 theta0$L[[7]] = c(.2,.39,.39,.01,.01)
 
-# Decrease first element of each probability by .001 so we don't start on the boundary
+# subtract 0.001 from first element of each vector to satisfy condition sum over ui < 1
 theta0$H = lapply(theta0$H, function(x){
   x[[1]] = x[[1]] - 0.001
   return(x)
