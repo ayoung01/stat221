1c6f40bac4a8ac6deccadf9755a294fcc8d6964d
Albert Tan Young <albert.t.young@gmail.com>
2014-10-03 23:11:14 -0400

    simYgivenTheta

--- pset2/YoungGentili_ps2.R
+++ pset2/YoungGentili_ps2.R
@@ -1,3 +1,11 @@
-simYgivenTheta(theta, w, N) = function() {
-
+# theta and w are of length J
+# N is a scalar
+# should return matrix Y
+simYgivenTheta = function(theta, w, n) {
+  J = length(theta)
+  Y = matrix(nrow = J, ncol = n)
+  for (j in 1:J) {
+    Y[j,] = rpois(n, w[j]*theta[j])
+  }
+  return(Y)
 }
