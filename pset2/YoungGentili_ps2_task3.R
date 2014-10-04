534ffbf16e5a0d8a828e7cd26bd6b75108105bc1
Albert Tan Young <albert.t.young@gmail.com>
2014-10-04 13:33:43 -0400

    fixed drawTheta bug

--- pset2/YoungGentili_ps2_task3.R
+++ pset2/YoungGentili_ps2_task3.R
@@ -1,8 +1,13 @@
 source('poissonLogN_MCMC.R')
 source('YoungGentili_ps2_functions.R')
 
-# Task 3 ------------------------------------------------------------------
+args <- as.numeric(commandArgs(trailingOnly = TRUE))
+if(length(args) != 1) {
+  stop("Not correct no. of args")
+}
+job.id = args[1]
 
+M = 80 # number of Ys to draw
 J = 1000
 w = rep(1, J)
 N = 2
@@ -13,23 +18,28 @@ params = list(c(mu=1.6,sd=0.7),c(mu=2.5,sd=1.3),c(mu=5.2,sd=1.3),c(mu=4.9,sd=1.6
 thetas = lapply(params, function(param){
   mu = param[1]
   sd = param[2]
-  drawTheta(J, mu, sd)
-})
-
-# Then you will repeatedly draw Y | Theta for a set of simulations
-Ys = lapply(thetas, function(theta){
-  simYgivenTheta(theta, w, N)
-})
-
-system.time(mcmc <- lapply(Ys, function(Y){
-  poisson.logn.mcmc(Y, w)
-}))
-# user  system elapsed
-# 38.732   3.729  42.477
-# It takes roughly 10.5 seconds to run MCMC
-
-coverage = list()
-for (i in 1:length(mcmc)) {
-  coverage[[i]] = CalculateCoverage(thetas[[i]], mcmc[[i]]$logTheta)
+  DrawTheta(J, mu, sd)
 }
+
+# create list of 4 to store coverage for each set of parameters
+coverage = list(rep(NA), 4)
+for (m in 1:M) {
+  sprintf('Repetition: %d', m)
+  # Then you will repeatedly draw Y | Theta for a set of simulations
+  Ys = lapply(thetas, function(theta){
+    simYgivenTheta(theta, w, N)
+  })
+
+  system.time(mcmc <- lapply(Ys, function(Y){
+    poisson.logn.mcmc(Y, w)
+  }))
+  # user  system elapsed
+  # 38.732   3.729  42.477
+  # It takes roughly 10.5 seconds to run MCMC
+  for (i in 1:length(mcmc)) {
+    coverage[[m]] = rbind(coverage[[m]], CalculateCoverage(thetas[[i]], mcmc[[i]]$logTheta))
+  }
+}
+
+save(coverage, file=sprintf("odyssey/coverage_%d.rda", job.id))
 
