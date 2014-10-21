64e6184485448774a7d275f0f8ea1c0e1d2e44c3
Albert Tan Young <ayoung@rclogin06.rc.fas.harvard.edu>
2014-10-21 10:23:44 -0400

    ran part 3 wo correct learning rate

---
+++ pset3/cmds/1000_5000_0.95.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH -J ps3_3 # name for job array
+#SBATCH -o all.out #Standard output
+#SBATCH -e all.err #Standard error
+#SBATCH -p serial_requeue #Partition
+#SBATCH --mem-per-cpu 4096 #Memory request
+#SBATCH -n 1 #Number of cores
+#SBATCH -N 1 #All cores on one machine
+Rscript ps3_YoungGentili_3.R 1000 5000 0.95 3
