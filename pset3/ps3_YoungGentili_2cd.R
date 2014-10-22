2d51a6fab82a3a792f58ac38ddb25d9b9adaab51
Albert Tan Young <albert.t.young@gmail.com>
2014-10-22 10:16:45 -0400

    fixed 2cd Rscript

--- pset3/ps3_YoungGentili_2cd.R
+++ pset3/ps3_YoungGentili_2cd.R
@@ -7,9 +7,6 @@ if(length(args) != 2) {
 n = args[1]
 alpha = args[2]
 
-n=100
-alpha=10
-
 res.sgd = c('sgd', run.sgd.2cd(nreps=100, alpha=alpha, n=n, p=100))
 res.asgd = c('asgd', run.sgd.2cd(nreps=100, alpha=alpha, n=n, p=100))
 res.implicit = c('implicit', run.sgd.2cd(nreps=100, alpha=alpha, n=n, p=100))
