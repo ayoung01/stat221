676a30119ea1f666c8e0a50eb0ef802fbeb1d372
Albert Tan Young <ayoung@rclogin06.rc.fas.harvard.edu>
2014-10-23 16:43:31 -0400

    finished running 2cd

---
+++ pset3/2cd_cmds/1e6_0.5.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH -J ps3_2cd # name for job array
+#SBATCH -o all.out #Standard output
+#SBATCH -e all.err #Standard error
+#SBATCH -p serial_requeue #Partition
+#SBATCH --mem-per-cpu 4096 #Memory request
+#SBATCH -n 1 #Number of cores
+#SBATCH -N 1 #All cores on one machine
+Rscript ps3_YoungGentili_2cd.R 1e6 0.5
