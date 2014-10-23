b421ddeb6a71a4bbbde8b335cdd500dd156ad7f5
Albert Tan Young <albert.t.young@gmail.com>
2014-10-23 19:04:22 -0400

    trying again

--- pset3/ps3_YoungGentili_2cd.R
+++ pset3/ps3_YoungGentili_2cd.R
@@ -1,21 +1,21 @@
 source('ps3_YoungGentili_2_functions.R')
 
 args <- as.numeric(commandArgs(trailingOnly = TRUE))
-if(length(args) != 2) {
+if(length(args) != 3) {
   stop("Not correct no. of args")
 }
 alpha = args[1]
 rep.no = args[2]
+n = args[3]
 
-res.sgd = cbind('sgd', rep.no, run.sgd.2cd(alpha=alpha))
-res.asgd = cbind('asgd', rep.no, run.sgd.2cd(alpha=alpha, asgd=T))
-res.implicit = cbind('implicit', rep.no, run.sgd.2cd(alpha=alpha, implicit=T))
-res = rbind(res.sgd, res.asgd, res.implicit)
-colnames(res)[1] = 'method'
-colnames(res)[2] = 'rep.no'
-
-write.table(res, file=sprintf("2cd/%.0f_%.0f.txt", alpha, rep.no),
+res = run.sgd.2cd(alpha=alpha, n=n)
+write.table(res, file=sprintf("2cd/sgd/%.2f_%.0f.txt", alpha, rep.no),
             row.names=FALSE, quote=FALSE)
 
+res = run.sgd.2cd(alpha=alpha, n=n, asgd=T)
+write.table(res, file=sprintf("2cd/asgd/%.2f_%.0f.txt", alpha, rep.no),
+            row.names=FALSE, quote=FALSE)
 
-
+res = run.sgd.2cd(alpha=alpha, n=n, implicit=T)
+write.table(res, file=sprintf("2cd/implicit/%.2f_%.0f.txt", alpha, rep.no),
+            row.names=FALSE, quote=FALSE)
