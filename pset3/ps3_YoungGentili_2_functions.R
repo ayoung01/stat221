e7d04ea39b096a1b5dc28a7b95eb29214241f489
Albert Tan Young <albert.t.young@gmail.com>
2014-10-23 18:16:30 -0400

    idk

--- pset3/ps3_YoungGentili_2_functions.R
+++ pset3/ps3_YoungGentili_2_functions.R
@@ -229,16 +229,13 @@ batch.sgd.2b <- function(data) {
 # 2cd ---------------------------------------------------------------------
 
 
-run.sgd.2cd <- function(alpha, n=1e5, p=100, asgd=F, implicit=F, verbose=T) {
+run.sgd.2cd <- function(alpha, n=1e6, p=100, asgd=F, implicit=F, verbose=T) {
   A = generate.A(p)
   data = sample.data(n, A)
 
   trace = sum(diag(data$A))
   theta.sgd = matrix(0, nrow=p, ncol=n)
   for (i in 1:(n-1)) {
-    if (n %% 10000 == 0 && verbose) {
-      print(n)
-    }
     theta.old = theta.sgd[, i]
     xi = data$X[i, ]
     ai = alpha / (alpha * trace + i)
--- pset3/ps3_YoungGentili_2cd.R
+++ pset3/ps3_YoungGentili_2cd.R
@@ -7,9 +7,9 @@ if(length(args) != 2) {
 alpha = args[1]
 rep.no = args[2]
 
-res.sgd = c('sgd', rep.no, run.sgd.2cd(alpha=alpha, n=n, p=100))
-res.asgd = c('asgd', rep.no, run.sgd.2cd(alpha=alpha, n=n, p=100, asgd=T))
-res.implicit = c('implicit', rep.no, run.sgd.2cd(alpha=alpha, n=n, p=100, implicit=T))
+res.sgd = c('sgd', rep.no, run.sgd.2cd(alpha=alpha))
+res.asgd = c('asgd', rep.no, run.sgd.2cd(alpha=alpha, asgd=T))
+res.implicit = c('implicit', rep.no, run.sgd.2cd(alpha=alpha, implicit=T))
 res = rbind(res.sgd, res.asgd, res.implicit)
 colnames(res)[1] = 'method'
 colnames(res)[2] = 'rep.no'
