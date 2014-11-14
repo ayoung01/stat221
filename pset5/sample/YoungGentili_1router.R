d14988b629a2c987be569b5c099c02a0ac065004
Albert Tan Young <albert.t.young@gmail.com>
2014-11-14 00:24:55 -0500

    finished 1.1 and 1.2

--- pset5/sample/YoungGentili_1router.R
+++ pset5/sample/YoungGentili_1router.R
@@ -1,8 +1,10 @@
 library(scales)
+router1 = read.csv("1router_allcount.dat")
+router2 = read.csv("2router_linkcount.dat")
+
 # 1.1 ---------------------------------------------------------------------
 
-links <- read.csv("1router_allcount.dat")
-links = links[grep("src|dst", links$nme),]
+links = router1[grep("src|dst", links$nme),]
 
 tmp = do.call(rbind, strsplit(links$nme, ' '))
 links$dir = tmp[, 1]
@@ -23,5 +25,30 @@ p + facet_grid(client ~ .)
 
 # 1.2 ---------------------------------------------------------------------
 
+# Router 1
+time.1130 = links[1065:1152,]
+mean.1130 = tapply(time.1130$value, time.1130$nme, function(x) {log10(mean(x))})
+var.1130 = tapply(time.1130$value, time.1130$nme, function(x) {log10(var(x))})
 
+time.1530 = links[1449:1536,]
+mean.1530 = tapply(time.1530$value, time.1530$nme, function(x) {log10(mean(x))})
+var.1530 = tapply(time.1530$value, time.1530$nme, function(x) {log10(var(x))})
 
+df = data.frame(x = c(mean.1130, mean.1530), y = c(var.1130, var.1530), panel = c(rep("time 11:30", 8), rep("time 15:30", 8)))
+p = ggplot(df, aes(x, y)) + geom_smooth(method="lm",data=df,colour="gray",se=FALSE, fullrange=TRUE)
+p + geom_point() + facet_grid(. ~ panel) + xlab('log10(mean)') + ylab('log10(var)') +
+  ggtitle('Local Variances Versus Local Means for Router1 Link Measurements')
+
+# Router 2
+time.1130 = router2[2129:2304,]
+mean.1130 = tapply(time.1130$value, time.1130$nme, function(x) {log10(mean(x))})
+var.1130 = tapply(time.1130$value, time.1130$nme, function(x) {log10(var(x))})
+
+time.1530 = links[2897:3072,]
+mean.1530 = tapply(time.1530$value, time.1530$nme, function(x) {log10(mean(x))})
+var.1530 = tapply(time.1530$value, time.1530$nme, function(x) {log10(var(x))})
+
+df = data.frame(x = c(mean.1130, mean.1530), y = c(var.1130, var.1530), panel = c(rep("time 11:30", 8), rep("time 15:30", 8)))
+p = ggplot(df, aes(x, y)) + geom_smooth(method="lm",data=df,colour="gray",se=FALSE, fullrange=TRUE)
+p + geom_point() + facet_grid(. ~ panel) + xlab('log10(mean)') + ylab('log10(var)') +
+  ggtitle('Local Variances Versus Local Means for Router2 Link Measurements')
