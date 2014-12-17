408cf81d90869b797013fb7c120706fbb3843456
Albert Tan Young <albert.t.young@gmail.com>
2014-12-16 21:14:04 -0800

    ran experiment on samples from karate network

--- pset6/novelty.R
+++ pset6/novelty.R
@@ -19,19 +19,23 @@ print('Predicted theta: '%+%ET.res[[n.samples]])
 
 plot(1:length(unlist(ET.res)), unlist(ET.res))
 
+# karate ------------------------------------------------------------------
+
 # karate data from http://www-personal.umich.edu/~mejn/netdata/
+# 34 nodes
+# 78 edges, 45 triangles, 528 two-stars
 library(igraph)
 
-g = read.graph('karate/karate.gml', format='gml')
-plot(g)
-g = get.adjacency(g, sparse=F)
+karate = read.graph('karate/karate.gml', format='gml')
+plot(karate)
+karate = get.adjacency(karate, sparse=F)
 
 n.iter = 1000
 theta0.ET = as.matrix(c(-1,-1))
 
 init.ET.G = vector('list', n.iter)
 for (i in 1:n.iter) {
-  init.ET.G[[i]] = g
+  init.ET.G[[i]] = karate
 }
 
 ET.res = SGD.Monte.Carlo(init.ET.G, init.ET.G[[1]], theta0.ET, ERGM.ET.ss,
@@ -39,3 +43,62 @@ ET.res = SGD.Monte.Carlo(init.ET.G, init.ET.G[[1]], theta0.ET, ERGM.ET.ss,
 print('Predicted theta: '%+%ET.res[[n.samples]])
 
 plot(1:length(unlist(ET.res)), unlist(ET.res))
+
+
+sample.subnetworks = function(g, n.samples, n.nodes) {
+  stopifnot (n.nodes < nrow(g))
+  samples = list()
+  for (i in 1:n.samples) {
+    g.samp = g
+
+    rows = sample(1:nrow(g), n.nodes, replace = F)
+    comp = (1:nrow(g))[!(1:nrow(g) %in% rows)]
+
+    g.samp[comp, ] = NA
+    g.samp[, comp] = NA
+    g.samp = g.samp[rowSums(is.na(g.samp)) != ncol(g.samp),]
+    g.samp = g.samp[, colSums(is.na(g.samp)) != nrow(g.samp)]
+    samples[[i]] = g.samp
+  }
+  return(samples)
+}
+
+n.nodes = 20
+n.samples = 1000
+
+# extract subnetwork of 20 random nodes
+karate.samples = sample.subnetworks(karate, n.samples = 1000, n.nodes = 20)
+
+# fit ET model
+karate.theta0.ET = as.matrix(c(1, -1))
+karate.ET.res = SGD.Monte.Carlo(karate.samples, karate.samples[[1]], karate.theta0.ET, ERGM.ET.ss,
+                         function(i) (1/(5+i)), n.draws=10, ERGM.ET.ss.diff, use.pkg=T, debug=F, verbose=T, model='ET')
+print('Predicted theta: '%+%karate.ET.res[[n.samples]])
+
+karate.ET.res = t(do.call(cbind, karate.ET.res))
+colnames(karate.ET.res) = c('edges', 'triangles')
+karate.ET.res = as.data.frame(karate.ET.res)
+karate.ET.res = stack(karate.ET.res)
+karate.ET.res$iteration = rep(1:n.samples, 2)
+colnames(karate.ET.res) = c('theta', 'stat', 'iteration')
+
+p = ggplot(karate.ET.res, aes(x=iteration, y=theta, colour=stat)) + geom_line()
+p + ggtitle('Predicted theta by iteration for ET model')
+
+
+# fit triad model
+karate.theta0.triad = as.matrix(c(-1, -1, -1))
+karate.triad.res = SGD.Monte.Carlo(karate.samples, karate.samples[[1]], karate.theta0.triad, ERGM.triad.ss,
+                                function(i) (1/(50+i)), n.draws=10, ERGM.triad.ss.diff, use.pkg=T, debug=F, verbose=T, model='triad')
+print('Predicted theta: '%+%karate.triad.res[[n.samples]])
+
+karate.triad.res = t(do.call(cbind, karate.triad.res))
+colnames(karate.triad.res) = c('edges', 'triangles', 'twostar')
+karate.triad.res = as.data.frame(karate.triad.res)
+karate.triad.res = stack(karate.triad.res)
+karate.triad.res$iteration = rep(1:n.samples, 3)
+colnames(karate.triad.res) = c('theta', 'stat', 'iteration')
+
+p = ggplot(karate.triad.res, aes(x=iteration, y=theta, colour=stat)) + geom_line()
+p + ggtitle('Predicted theta by iteration for triad model')
+
